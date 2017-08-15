c022ed2275a4a27667502e062596b9931c5f6fdf
Alana Marzoev <alanamarzoev@Alanas-MacBook-Pro.local>
2017-08-14 22:36:25 -0700

    lol

--- tutorials/image/cifar10/cifar10_multi_gpu_train.py
+++ tutorials/image/cifar10/cifar10_multi_gpu_train.py
@@ -120,6 +120,7 @@ def average_gradients(tower_grads):
     #   ((grad0_gpu0, var0_gpu0), ... , (grad0_gpuN, var0_gpuN))
     grads = []
     for g, _ in grad_and_vars:
+      print("g " + str(g))
       # Add 0 dimension to the gradients to represent the tower.
       expanded_g = tf.expand_dims(g, 0)
 
