8ee59c2a744b360b8fa9ce351bf8fb96930cd35d
Alana Marzoev <alanamarzoev@dhcp-rhodes-92.eduroam.cornell.edu>
2017-08-27 15:07:44 -0400

    hi

--- tutorials/image/cifar10/cifar10_multi_gpu_train.py
+++ tutorials/image/cifar10/cifar10_multi_gpu_train.py
@@ -149,11 +149,12 @@ def average_gradients(tower_grads):
 
 def eval_once(saver, summary_writer, top_k_op, global_step):
     #top_k_op = tf.nn.in_top_k(logits, labels, 1)
-    total_sample_count = FLAGS.batch_size 
+    total_sample_count = FLAGS.batch_size
     true_count = 0
     num_got = tf.reduce_sum(tf.cast(top_k_op, tf.float32))
     true_count += num_got
     precision = true_count / total_sample_count
+    print("PRECISION " + str(precision))
     print('%s: precision @ 1 = %.3f' % (datetime.now(), precision))
 
     #   step = 0
